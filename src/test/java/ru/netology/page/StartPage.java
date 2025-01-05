547e9511fdb5de61bcc9607da53f7fe693457dde
Netotest <athomson@mail.ru>
2025-01-05 15:39:07 +0300

    cant start tests

--- src/test/java/ru/netology/page/StartPage.java
+++ src/test/java/ru/netology/page/StartPage.java
@@ -5,12 +5,12 @@ import static com.codeborne.selenide.Selenide.$$;
 
 public class StartPage {
     public BuyPage buyPage() {
-        $$("button.button_size_m").find(exactText("Купить")).click();
+        $$("button button_size_m button_theme_alfa-on-white").find(exactText("Купить")).click();
         return new BuyPage();
     }
 
     public BuyCreditPage buyCreditPage() {
-        $$("[button__content]").find(exactText("Купить в кредит")).click();
+        $$("[button button_view_extra button_size_m button_theme_alfa-on-white]").find(exactText("Купить в кредит")).click();
         return new BuyCreditPage();
     }
 }
